3753d1dadaa237bbbef72198880cd35a261fba93
Soukrat55 <fleursoukrat@gmail.com>
2024-01-24 07:02:17 +0100

    seconde commit

--- angular-test/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppModule } from './app/app.module';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, AppModule)
-  .catch((err) => console.error(err));
